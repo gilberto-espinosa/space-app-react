46c5d318dea05ba1e98e0bb3cb0051e5f4172c65
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2024-02-17 04:26:05 -0300

    BarraLateral opções adicionadas

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -6,6 +6,14 @@ import homeInativo from "/icones/home-inativo.png";
 import vistasAtivo from "/icones/mais-vistas-ativo.png";
 import vistasInativo from "/icones/mais-vistas-inativo.png";
 
+import curtidaAtivo from "/icones/mais-curtidas-ativo.png";
+import curtidaInativo from "/icones/mais-curtidas-inativo.png";
+import surpreendaAtivo from "/icones/surpreenda-me-ativo.png";
+import surpreendaInativo from "/icones/surpreenda-me-inativo.png";
+import novasAtivo from "/icones/novas-ativo.png";
+import novasInativo from "/icones/novas-inativo.png";
+
+
 const ListaEstilizada = styled.ul`
   list-style: none;
   padding: 0;
@@ -30,7 +38,28 @@ const BarraLateral = () => {
             iconeInativo={vistasInativo}
             ativo={false}
           >
-            Mais Visitados
+            Mais vistas
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo={curtidaAtivo}
+            iconeInativo={curtidaInativo}
+            ativo={false}
+          >
+            Mais curtidas
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo={novasAtivo}
+            iconeInativo={novasInativo}
+            ativo={false}
+          >
+            Novas
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo={surpreendaAtivo}
+            iconeInativo={surpreendaInativo}
+            ativo={false}
+          >
+            Surpreenda-me
           </ItemNavegacao>
         </ListaEstilizada>
       </nav>
